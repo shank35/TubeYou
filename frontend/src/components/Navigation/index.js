fe9216a092b65c1d41049a5ea8d088409dc807f0
Shan Kim <shankim35@gmail.com>
2023-04-04 15:26:15 -0400

    finished sidebar icons

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import youtubeLogo from "../../assets/icons/youtube_logo.png";
 import search from "../../assets/icons/search.png";
@@ -10,6 +10,9 @@ import "./Navigation.css";
 function Navigation() {
   const [navVisible, setNavVisible] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
+  const [profileDropdownVisible, setProfileDropdownVisible] = useState(false);
+
+  const [users, setUsers] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,6 +28,33 @@ function Navigation() {
       setNavVisible(false);
     }
   };
+
+  const toggleProfileDropdown = () => {
+    setProfileDropdownVisible(!profileDropdownVisible);
+  };
+
+  const profileOutsideCLick = (e) => {
+    if (!e.target.closest('.profileDropdown') && !e.target.closest('.profileContainer')) {
+      setProfileDropdownVisible(false);
+    }
+  };
+  
+  useEffect(() => {
+    document.addEventListener('click', profileOutsideCLick);
+    return () => {
+      document.removeEventListener('click', profileOutsideCLick);
+    };
+  }, []);
+
+  useEffect(() => {
+    fetch('/users.json')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data);
+      });
+  }, []);
+  
+  
 
   return (
     <div className="body">
@@ -48,62 +78,62 @@ function Navigation() {
                   {/* Add YouTube-like sidebar menu items here */}
                   <ul className="sidebarMenu">
                     <li className="sidebarMenuItem">
-                    <i className="fa-sharp fa-solid fa-house sidebarMenuIcon"></i>
-                      Home
+                      <span className="material-symbols-outlined sidebarMenuIcon">home</span>
+                      <button className="sidebarText">Home</button>                      
                     </li>
                     <li className="sidebarMenuItem">
-                      <i class="fa-solid fa-address-book sidebarMenuIcon"></i>
-                      Subscriptions
-                    </li>
-                    <li className="sidebarLine"></li>
-                    <li className="sidebarMenuItem">
-                    <i className="fa-solid fa-book sidebarMenuIcon"></i>
-                      Library
-                      </li>
-                    <li className="sidebarMenuItem">
-                    <i className="fa-solid fa-clock-rotate-left sidebarMenuIcon"></i>
-                      History
-                    </li>
-                    <li className="sidebarMenuItem">
-                      <i className="fa-solid fa-video sidebarMenuIcon"></i>
-                      Your Videos
-                    </li>
-                    <li className="sidebarMenuItem">
-                      <i className="fa-solid fa-clock sidebarMenuIcon"></i>
-                      Watch Later
-                    </li>
-                    <li className="sidebarMenuItem">
-                      <i className="fa-solid fa-thumbs-up sidebarMenuIcon"></i>
-                      Like Videos
+                      <span className="material-symbols-outlined sidebarMenuIcon">subscriptions</span>
+                      <button className="sidebarText">Subscriptions</button>
                     </li>
                     <li className="sidebarLine"></li>
-                    <li className="sidebarHeader sidebarMenuIcon">
-                      Subscriptions
+                    <li className="sidebarMenuItem">
+                      <span class="material-symbols-outlined sidebarMenuIcon">video_library</span>
+                      <button className="sidebarText">Library</button>
                     </li>
                     <li className="sidebarMenuItem">
-                      <i className="fa-solid fa-user sidebarMenuIcon"></i>
-                      Channel 1
+                      <span class="material-symbols-outlined sidebarMenuIcon">history</span>
+                      <button className="sidebarText">History</button>
                     </li>
                     <li className="sidebarMenuItem">
-                      <i className="fa-solid fa-user sidebarMenuIcon"></i>
-                      Channel 2
+                      <span class="material-symbols-outlined sidebarMenuIcon">smart_display</span>
+                      <button className="sidebarText">Your Videos</button>
                     </li>
                     <li className="sidebarMenuItem">
-                      <i className="fa-solid fa-circle-plus sidebarMenuIcon"></i>
-                      Browse Channels
+                      <span class="material-symbols-outlined sidebarMenuIcon">schedule</span>
+                      <button className="sidebarText">Watch Later</button>
+                    </li>
+                    <li className="sidebarMenuItem">
+                      <span class="material-symbols-outlined sidebarMenuIcon">thumb_up</span>
+                      <button className="sidebarText">Like Videos</button>
                     </li>
                     <li className="sidebarLine"></li>
                     <li className="sidebarHeader sidebarMenuIcon">
-                      Subscriptions
+                      <span className="sidebarText">Subscriptions</span>
                     </li>
                     <li className="sidebarMenuItem">
-                      <i className="fa-solid fa-fire sidebarMenuIcon"></i>
-                      Trending
+                      <span class="material-symbols-outlined sidebarMenuIcon">person</span>
+                      <button className="sidebarText">Channel 1</button>
+                    </li>
+                    <li className="sidebarMenuItem">
+                      <span class="material-symbols-outlined sidebarMenuIcon">person</span>
+                      <button className="sidebarText">Channel 2</button>
+                    </li>
+                    <li className="sidebarMenuItem">
+                      <span class="material-symbols-outlined sidebarMenuIcon">add_circle</span>  
+                      <button className="sidebarText">Browse Channels</button>
+                    </li>
+                    <li className="sidebarLine"></li>
+                    <li className="sidebarHeader sidebarMenuIcon">
+                      <span className="sidebarText">Explore</span>
+                    </li>
+                    <li className="sidebarMenuItem">
+                      <span class="material-symbols-outlined sidebarMenuIcon">local_fire_department</span>
+                      <button className="sidebarText">Trending</button>                 
                     </li>
                     <li className="sidebarLine"></li>
                     <li className="sidebarMenuItem">
-                      <i className="fa-solid fa-gear sidebarMenuIcon"></i>
-                      Settings
+                      <span className="material-symbols-outlined sidebarMenuIcon">settings</span>                    
+                      <button className="sidebarText">Settings</button>
                     </li>
                   </ul>
                 </div>
@@ -135,9 +165,31 @@ function Navigation() {
               <img className="upload" src={upload} alt="Upload" />
             </a>
 
-            <a href="/">
-              <img className="upload" src={profileDefault} alt="Profile" />
-            </a>
+            <div className="profile">
+              <button className="profileContainer" onClick={toggleProfileDropdown}>
+                <img className="profileImage" src={profileDefault} alt="Profile" />
+              </button>
+              {profileDropdownVisible && (
+                <div className="profileDropdown">
+                  <div className="profileHeader">
+                    <img className="profileImage" src={profileDefault} alt="Profile" />
+                    <div className="profileInfo">
+                      <p className="profileName">John Doe</p>
+                      <p className="profileEmail">john.doe@example.com</p>
+                    </div>
+                  </div>
+                  <button className="dropdownButton">Your channel</button>
+                  <button className="dropdownButton">Sign out</button>
+                  <hr className="dropdownDivider" />
+                  <button className="dropdownButton">Dark mode</button>
+                  <hr className="dropdownDivider" />
+                  <button className="dropdownButton">Settings</button>
+                </div>
+              )}
+            </div>
+                
+
+
           </div>
         </div>
 
