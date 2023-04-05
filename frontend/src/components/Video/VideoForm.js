4f25e2a27f7f6d3428f7e720b5edc9af8714047c
Shan Kim <shankim35@gmail.com>
2023-04-05 18:00:10 -0400

    finished upload video modal

--- frontend/src/components/Video/VideoForm.js
+++ frontend/src/components/Video/VideoForm.js
@@ -24,11 +24,11 @@ function VideoForm() {
           </div>
 
           <div className="titleContainer">
-            <input className="form-control" type="text" placeholder="Title" name="titleInput" />
+            <input className="title-form-control" type="text" placeholder="Title" name="titleInput" />
           </div>
 
           <div className="descriptionContainer">
-            <textarea className="form-control" placeholder="Description" name="descriptionInput" rows="3"></textarea>
+            <textarea className="description-form-control" placeholder="Description" name="descriptionInput" rows="3"></textarea>
           </div>
 
           {/* <div className="formControlContainer">
@@ -46,23 +46,47 @@ function VideoForm() {
 
           {showModal && (
             <div className="modalVideoContainer">
+
               <div className="modalVideoOverlay" onClick={handleCloseModal}></div>
+
               <div className="modalVideoContent">
+
                 <div className="modalVideoHeader">
                   <h2>Upload video</h2>
                   <button className="closeVideoModalButton" onClick={handleCloseModal}>×</button>
                 </div>
+
                 <div className="modalVideoBody">
+
                   <div className="modalVideoUploadArea">
+
                     <div className="modalVideoUploadIconContainer">
-                      <span class="material-symbols-outlined" style={{fontSize: "50px"}}>upload</span>                    </div>
-                    <div className="modalVideoUploadTextContainer">
-                      <p>Select files to upload</p>
-                      <input type="file" className="form-control-file" id="exampleFormControlFile1" name="fileInput" required />
+                      <span class="material-symbols-outlined" style={{fontSize: "50px"}}>upload</span>                    
                     </div>
+
+                    <div className="modalVideoText1">
+                      <p>Drag and drop video files to upload</p>
+                    </div>
+
+                    <div className="modalVideoText2">
+                      <p>Your videos will be private until you publish them.</p>
+                    </div>
+
+                    <div className="modalVideoUploadTextContainer">
+                      <div className="uploadLabelContainer">
+                        <label htmlFor="fileInput" className="custom-file-upload">
+                          <p>Select files</p>
+                        </label>
+                        <input type="file" id="fileInput" name="fileInput" required />
+                      </div>
+                    </div>
+                    
                   </div>
+
                 </div>
+
               </div>
+
             </div>
           )}
 
