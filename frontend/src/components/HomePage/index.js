c23a987c994c4c1f0a5d97a85c8612d4ce20c4a9
Shan Kim <shankim35@gmail.com>
2023-04-05 12:09:23 -0400

    made videos placeholders on the home page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,28 +1,65 @@
-import React, { useState, useRef, useEffect } from "react";
-import menuImage from "../../assets/icons/menu.png";
-import youtubeLogo from "../../assets/icons/youtube_logo.png";
-import search from "../../assets/icons/search.png";
-import profileDefault from "../../assets/profile/default.png";
-import upload from "../../assets/icons/upload.png";
+import React from "react";
+import video1 from "./video1.png";
 
 import "./reset.css";
 import "./HomePage.css";
 
 function HomePage() {
-
-
   return (
     <div className="body">
       <div id="page">
-        <div id="mastHead">
-
-          <div id="mainSectionContainer">
-            {/* Main Section */}
+        <div id="mainSectionContainer">
+          <div className="videoPlaceholder">
+            <img src={video1} alt="video thumbnail" />
+            <div className="videoInfo">
+              <h3 className="videoTitle">Video Title</h3>
+              <p className="videoChannel">Channel Name</p>
+              <p className="videoViews">1.5M views • 1 week ago</p>
+            </div>
+          </div>
+          <div className="videoPlaceholder">
+          <img src={video1} alt="video thumbnail" />
+            <div className="videoInfo">
+              <h3 className="videoTitle">Video Title</h3>
+              <p className="videoChannel">Channel Name</p>
+              <p className="videoViews">3.2M views • 2 weeks ago</p>
+            </div>
+          </div>
+          <div className="videoPlaceholder">
+          <img src={video1} alt="video thumbnail" />
+            <div className="videoInfo">
+              <h3 className="videoTitle">Video Title</h3>
+              <p className="videoChannel">Channel Name</p>
+              <p className="videoViews">2.1M views • 3 weeks ago</p>
+            </div>
+          </div>
+          <div className="videoPlaceholder">
+          <img src={video1} alt="video thumbnail" />
+            <div className="videoInfo">
+              <h3 className="videoTitle">Video Title</h3>
+              <p className="videoChannel">Channel Name</p>
+              <p className="videoViews">4.5M views • 4 weeks ago</p>
+            </div>
+          </div>
+          <div className="videoPlaceholder">
+            <img src={video1} alt="video thumbnail" />
+            <div className="videoInfo">
+              <h3 className="videoTitle">Video Title</h3>
+              <p className="videoChannel">Channel Name</p>
+              <p className="videoViews">1.2M views • 5 weeks ago</p>
+            </div>
+          </div>
+          <div className="videoPlaceholder">
+            <img src={video1} alt="video thumbnail" />
+            <div className="videoInfo">
+              <h3 className="videoTitle">Video Title</h3>
+              <p className="videoChannel">Channel Name</p>
+              <p className="videoViews">2.8M views • 6 weeks ago</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
   );
 }
 
