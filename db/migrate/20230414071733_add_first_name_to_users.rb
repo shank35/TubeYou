fc395b2d901c870c9810ac1562e387c405b85905
Shan Kim <shankim35@gmail.com>
2023-04-14 03:19:15 -0400

    updated first name

---
+++ db/migrate/20230414071733_add_first_name_to_users.rb
@@ -0,0 +1,7 @@
+class AddFirstNameToUsers < ActiveRecord::Migration[7.0]
+  def change
+    unless column_exists?(:users, :first_name)
+      add_column :users, :first_name, :string
+    end
+  end
+end
