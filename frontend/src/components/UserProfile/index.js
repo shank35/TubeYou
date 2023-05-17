097a66e6c8eee28d6a240d294490200a6a0d418c
Shan Kim <shankim35@gmail.com>
2023-05-17 01:29:48 -0400

    fixed profile color changing

--- frontend/src/components/UserProfile/index.js
+++ frontend/src/components/UserProfile/index.js
@@ -1,14 +1,30 @@
 //frontend/src/UserProfile/index.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useSelector } from 'react-redux';
 import "./UserProfile.css"
+
 
 function UserAvatar() {
   const user = useSelector(state => state?.session.user);
 
   const letter = user.username.substr(0, 1).toUpperCase();
-  const backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16); // generate a random color for each user
+
+  const [backgroundColor, setBackgroundColor] = useState('');
+  
+  // only generate a new color when the component is first mounted (or when the user changes)
+  useEffect(() => {
+    // try to load the color from local storage
+    const savedColor = localStorage.getItem(`user-color-${user.id}`);
+    if (savedColor) {
+      setBackgroundColor(savedColor);
+    } else {
+      const newColor = '#' + Math.floor(Math.random()*16777215).toString(16); // generate a random color for each user
+      setBackgroundColor(newColor);
+      localStorage.setItem(`user-color-${user.id}`, newColor); // save the color in local storage
+    }
+  }, [user]);
+
   return (
     <div className="user-avatar" style={{backgroundColor}}>
       {letter}
