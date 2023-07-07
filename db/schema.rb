51e80915e613896f24bc204d2463979340dd2286
Shan Kim <shankim35@gmail.com>
2023-07-07 05:32:34 -0400

    removed first name and last name null false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_071733) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_092852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,8 +66,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_071733) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
     t.string "username", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
